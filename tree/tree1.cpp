b36239e6a80eda1197102b1818f9b922ceb2161e
ankur <ankur977@gmail.com>
2018-06-06 00:52:46 +0530

    -

--- tree/tree1.cpp
+++ tree/tree1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct node
 {
-	ll data;
+	ll data,lh,rh;
 	struct node* left;
 	struct node* right;
 	
@@ -16,6 +16,18 @@ struct node* addnode(int val){
 	return tmp;
 }
 
+int finddia(struct node* ptr){
+	ll max_value = INT_MIN;
+	if(ptr){
+		ptr->lh = finddia(ptr->left);
+		ptr->rh = finddia(ptr->right);
+		if(ptr->rh+ptr->lh>max_value)
+			max_value=ptr->rh+ptr->lh;
+
+		return (ptr->rh > ptr->lh ? ptr->rh:ptr->lh)+1;
+	}
+}
+
 
 
 int main(){
@@ -23,8 +35,31 @@ int main(){
 	cin>>num>>vroot;
 	struct node *root=NULL,*ptr;
 	root=addnode(vroot);
-	string s;
-		
+	string str;
+	while(num--){
+		ll i=0,value;
+		ptr=root;
+		cin>>str;
+		while(ptr&&str[i]){
+			if(str[i]=='L'){
+				if(ptr->left==NULL)
+					ptr->left=addnode(0);
+				ptr=ptr->left;
+			}
+			else{
+				if(ptr->right==NULL)
+					ptr->right=addnode(0);
+				ptr=ptr->right;
+			}
+			i++;
+			cin>>value;
+			ptr->data= value;
+		}
+	}
+
+	ll ans=		finddia(root);
+
+	cout<<ans;
 
 
 
