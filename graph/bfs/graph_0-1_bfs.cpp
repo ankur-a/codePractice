586c313f70f34eaccb8b573b058acc655cd01f1e
ankur <ankur977@gmail.com>
2018-06-14 16:19:39 +0530

    -

--- graph/bfs/graph_0-1_bfs.cpp
+++ graph/bfs/graph_0-1_bfs.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 typedef long long ll;
-std::vector < pair(ll,ll) > v;
-ll v;
+using namespace std;
+std::vector < pair<ll,ll> > v[20];
+ll nodes;
 void bfs_zero(ll s){
-	int dist[v];
-	memset(dist,INT_MAX,sizeof(dist));
+	int dist[nodes];
+	for(ll i=0 ; i<nodes; i++)
+		dist[i]=INT_MAX;
 	deque <ll> q;
 	dist[s] = 0;
-	q.push_back(src);
+	q.push_back(s);
 	while(!q.empty()){
 		ll tmp = q.front();
 		q.pop_front();
 
 		for(ll i=0;i< v[tmp].size();i++){
-			if(dist[v[tmp][i].first] > dist[v] + v[tmp][i].second)
+			if(dist[v[tmp][i].first] > dist[tmp] + v[tmp][i].second)
 				{
-					dist[v[tmp][i].first] > dist[v] + v[tmp][i].second;
+					dist[v[tmp][i].first] = (dist[tmp] + v[tmp][i].second) ;
 
 					if(v[tmp][i].second==0)
 						q.push_front(v[tmp][i].first);
@@ -25,15 +27,23 @@ void bfs_zero(ll s){
 		}
 	}
 
-	for(ll i=0;i<v; i++)
-		cout<<dist[v]<<" "<<endl;
+	for(ll i=0;i<nodes; i++)
+		cout<<dist[i]<<" "<<endl;
 }
-using namespace std;
+
 int main(){
-	std::ios::sync_with_stdio(false);
-	ll edges,weight;
-	cin>>v>>edges;
-	for()
+
+	ll edges,weight,j,k,wt;
+	cin>>edges>>nodes;
+	for(ll i=0;i<edges;i++){
+		cin>>j>>k>>wt;
+		v[j].push_back(make_pair(k,wt));
+		v[k].push_back(make_pair(j,wt));
+	}
+
+	bfs_zero(0);
+
+	
 	return 0;
 
 }
